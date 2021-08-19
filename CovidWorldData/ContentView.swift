d47481a38f8f643f5dfb0b4c05bfcd96d0427bc6
Maginease <minseoowen@gmail.com>
2021-08-19 14:43:22 +0900

    Initial Commit

---
+++ CovidWorldData/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CovidWorldData
+//
+//  Created by Minseo Kim on 8/19/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CovidWorldData/CovidWorldDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  CovidWorldDataApp.swift
+//  CovidWorldData
+//
+//  Created by Minseo Kim on 8/19/21.
+//
+
+import SwiftUI
+
+@main
+struct CovidWorldDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
